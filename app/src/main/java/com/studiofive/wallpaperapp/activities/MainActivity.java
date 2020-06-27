f4ae40bb123df75419a811c92e115cba0468f6b8
Samuel-dot-cloud <successam336@gmail.com>
2020-06-27 18:38:16 +0300

    Basic setup of activities and fragments.

---
+++ app/src/main/java/com/studiofive/wallpaperapp/activities/MainActivity.java
@@ -0,0 +1,99 @@
+package com.studiofive.wallpaperapp.activities;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.Menu;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.navigation.NavigationView;
+import com.studiofive.wallpaperapp.R;
+import com.studiofive.wallpaperapp.fragments.CollectionsFragment;
+import com.studiofive.wallpaperapp.fragments.FavouriteFragment;
+import com.studiofive.wallpaperapp.fragments.PhotosFragments;
+import com.studiofive.wallpaperapp.utils.Functions;
+
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.core.view.GravityCompat;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+
+    private AppBarConfiguration mAppBarConfiguration;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.nav_photos) {
+            PhotosFragments photosFragment = new PhotosFragments();
+            Functions.changeMainFragment(MainActivity.this, photosFragment);
+        } else if (id == R.id.nav_collections) {
+            CollectionsFragment collectionsFragment = new CollectionsFragment();
+            Functions.changeMainFragment(MainActivity.this, collectionsFragment);
+        } else if (id == R.id.nav_favourite) {
+            FavouriteFragment favoriteFragment = new FavouriteFragment();
+            Functions.changeMainFragment(MainActivity.this, favoriteFragment);
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+
+}
---
+++ app/src/main/java/com/studiofive/wallpaperapp/utils/Functions.java
@@ -0,0 +1,15 @@
+package com.studiofive.wallpaperapp.utils;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+
+import com.studiofive.wallpaperapp.R;
+
+public class Functions {
+    public static void changeMainFragment(FragmentActivity fragmentActivity, Fragment fragment){
+        fragmentActivity.getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.main_container, fragment)
+                .commit();
+    }
+}
