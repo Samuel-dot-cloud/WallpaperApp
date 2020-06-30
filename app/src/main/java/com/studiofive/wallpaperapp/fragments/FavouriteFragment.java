0a23b7537f0546a28e8094297a79a9aadd09745a
Samuel-dot-cloud <successam336@gmail.com>
2020-06-30 20:43:29 +0300

    Adding a feature to view favourite photos

--- app/src/main/java/com/studiofive/wallpaperapp/fragments/FavouriteFragment.java
+++ app/src/main/java/com/studiofive/wallpaperapp/fragments/FavouriteFragment.java
@@ -4,23 +4,66 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.studiofive.wallpaperapp.Adapters.PhotosAdapter;
+import com.studiofive.wallpaperapp.Models.Photo;
 import com.studiofive.wallpaperapp.R;
+import com.studiofive.wallpaperapp.utils.RealmController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
 
 public class FavouriteFragment extends Fragment {
+    @BindView(R.id.fragment_favourite_recyclerview)
+    RecyclerView recyclerView;
+    @BindView(R.id.fragment_favourite_notification)
+    TextView notification;
+
+    private PhotosAdapter photosAdapter;
+    private List<Photo> photos = new ArrayList<>();
+    private Unbinder unbinder;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_favourites, container, false);
+        unbinder = ButterKnife.bind(this, view);
+
+        // RecyclerView
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getActivity());
+        recyclerView.setLayoutManager(linearLayoutManager);
+        photosAdapter = new PhotosAdapter(getActivity(), photos);
+        recyclerView.setAdapter(photosAdapter);
+        
+        getPhotos();
         return view;
+    }
+
+    private void getPhotos() {
+        RealmController realmController = new RealmController();
+        photos.addAll(realmController.getPhotos());
+        if (photos.size() == 0){
+            notification.setVisibility(View.VISIBLE);
+            recyclerView.setVisibility(View.GONE);
+        }else {
+            photosAdapter.notifyDataSetChanged();
+        }
     }
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
+        unbinder.unbind();
     }
 }
